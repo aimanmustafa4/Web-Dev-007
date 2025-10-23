879e79df89d3a7e93d93f92252be0af9b8ac8111
Aiman Mustafa <aimenmustafa1828630@gmail.com>
2025-10-23 19:32:05 +0500

    js-learning

---
+++ 02_operators.js
@@ -0,0 +1,42 @@
+console.log("hello operators");
+
+// operators are  the symbols use to perform specific functions 
+// 4 types of operators
+// 1-mathematical(arrithmetic operators)
+// 2-comparison operators
+// == check value not data type
+// === check both
+
+
+// ? arithematic operators
+
+let number1 = 3;
+let number2 = 7;
+  
+let sum = number1 + number2;
+console.log(sum);
+
+let difference = number2 - number1;
+console.log(difference);
+let multiply = number2 * number1;
+console.log(multiply);
+let divide = number2/number1;
+console.log(divide);
+let modulus = number1 % number2 ;
+console.log( " the modulus of this operatorr" , modulus);
+
+
+// ? comparison 
+
+let age = "20"; 
+let canDrive = age >= 18;
+console.log(canDrive);
+
+
+
+
+
+
+
+
+;
