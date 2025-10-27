60f3d645a35a5341ed53b2c88eb27835488cc405
Aiman Mustafa <aimenmustafa1828630@gmail.com>
2025-10-27 22:11:13 +0500

    assignment and logical operators

---
+++ js/operatrs2.js
@@ -0,0 +1,23 @@
+console.log("hello");
+
+// assignment operators
+
+let a = 28;
+let b = 9;
+let result = a%=b; //a = a+10
+console.log(result);
+
+let salary = 1500;
+console.log("the salary is ", salary);
+let increment = 1200;
+salary*=increment;
+console.log("now, the salary is ",salary);
+
+// ?logical 
+let p = 10;
+let q = 20;
+let cond1 = p < q;
+let cond2 = q < p;
+console.log("cond1 = cond2", cond1 = cond2);
+
+
