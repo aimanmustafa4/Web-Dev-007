5d5b98d7699e9566c383a29ae6c9af9c5f5f5d21
Aiman Mustafa <aimenmustafa1828630@gmail.com>
2025-11-11 19:29:49 +0500

    array completed

---
+++ js/array2.js
@@ -0,0 +1,61 @@
+console.log("Code with ayaz");
+let arr=[9,1,2,3]; // syntax
+
+for( let digits of arr){
+    console.log(digits);
+    
+}
+
+let dataOfWholeNumber=["string" ,"integer" , "variable"];
+// dataOfWholeNumber.shift();
+
+console.log(dataOfWholeNumber.toString());
+// dataOfWholeNumber.shift();
+// console.log(dataOfWholeNumber);
+
+
+
+
+
+
+// console.log(dataOfWholeNumber);
+// console.log(dataOfWholeNumber.indexOf(4));
+// console.log(dataOfWholeNumber.length);
+// console.log(dataOfWholeNumber.reverse());
+// how add data in the array with associtaive array
+
+let userData=["hassan",23,"123",true];
+console.log(userData);
+for (const xyz of userData) {
+    console.log(xyz);
+    
+    
+}
+let firstArray=[1,2,3,4,5];
+let secondArray=[6,7,8,9,0];
+let combo=firstArray.concat(secondArray);
+console.log(combo.toString());
+
+
+
+let names = ["hamza","hassan","zamin","hashmat","usama","saim","ayaz","fahad","subhan","malaika"];
+
+
+// console.log(names.slice(2,7));
+names.splice(1 ,7,"ali" );
+console.log(names);
+
+ let assocArray =[];
+ assocArray.name= "Ayaz"; //index is 0
+ assocArray[1]= "Alipur";
+ assocArray['age']= 23;
+ assocArray['nationality']="pakistan";
+ console.log(assocArray.name);
+ console.log(assocArray[1]);
+ console.log(assocArray['city']);
+ console.log(assocArray['nationality']);
+
+
+
+
+
