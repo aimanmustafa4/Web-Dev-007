2bbfb9206b3d8e70b20afee850ce494a2098888c
Aiman Mustafa <aimenmustafa1828630@gmail.com>
2025-12-05 19:25:04 +0500

    eventlistener

--- dom.js
+++ dom.js
@@ -1,8 +1,8 @@
 console.log("Learning DOM");
 // OLD WAY
-let paraHead = document.getElementsByClassName("head");
-paraHead[0].innerHTML = "WEB DEV 7";
-console.log(paraHead);
+// let paraHead = document.getElementsByClassName("head");
+// paraHead[0].innerHTML = "WEB DEV 7";
+// console.log(paraHead);
 
 // let buttonSelect = document.getElementById("small");
 // buttonSelect.innerHTML = "Add here"
@@ -10,6 +10,31 @@ console.log(paraHead);
 
 
 //new way
-let buttonElect = document.querySelectorAll(".para");
-buttonElect[0].innerHTML = "Add here";
-console.log(buttonElect);
+// let buttonElect = document.querySelectorAll(".para");
+// buttonElect[0].innerHTML = "Add here";
+// console.log(buttonElect);
+
+let buttonSelect = document.querySelector("#btn");
+// buttonSelect.onmouseover = (e) =>{
+//     console.log (e);
+//     console.log (e.type);
+//     console.log (e.target);
+//     console.log (e);
+//     alert("heloo");
+
+// }
+//  node.addeventlistner (event , function){}
+buttonSelect.addEventListener("click", ()=>{
+    console.log("button is submitted");
+    
+})
+
+let paraHead = document.querySelector(".head");
+paraHead.onclick = (evt) =>{
+    console.log (evt);
+    console.log (evt.type);
+    console.log (evt.target);
+    console.log ("this is a head");
+}
+
+
