7b86dd4ab13aa23cd6f43a3b8c407c4c320a4fc5
Aiman Mustafa <aimenmustafa1828630@gmail.com>
2025-12-17 09:45:46 +0500

    folderr structure revamped

---
+++ backend/index.php
@@ -0,0 +1,36 @@
+<?php
+$userName = "ali";
+$userAge  = 30;
+
+echo "hy my name is $userName  and my age is $userAge ";
+
+
+
+
+echo "<br>";
+
+$firstName = "Zamin";
+$lastName = "Hussain";
+$fullName = $firstName." ".$lastName;
+echo $fullName ;
+
+
+
+echo "<br>";
+
+$userLogin = false;
+if($userLogin){
+    echo "u are already login.welcome";
+}else{
+    echo "u are not  login.";
+}
+
+echo "<br>";
+
+$personDetails =[
+    "name" => "Usama Mehboob",
+    "age" => 19,
+    "city" => "hasilpur"
+];
+echo "my name is $personDetails[name]";
+?>
